7b10f939209cf3a84e14c348ab46167ed330d558
Aishwarya <aishwarya.rao0509@gmail.com>
2024-02-02 11:03:16 +0100

    Retrieving lon and lat from city name using third party api

--- weather/views.py
+++ weather/views.py
@@ -39,6 +39,26 @@ def get_wind_direction(wind_direction):
     return None  # Invalid wind direction
 
 
+def get_lat_lon_from_nominatim(city):
+    
+    nominatim_api_url = f"https://nominatim.openstreetmap.org/search.php?q={city}&format=jsonv2"
+
+    try:      
+        response = requests.get(nominatim_api_url)
+        response.raise_for_status()
+        long_lat_data = response.json()
+
+        if long_lat_data:
+            lat = float(long_lat_data[0]['lat'])
+            lon = float(long_lat_data[0]['lon'])
+            return lat, lon
+        else:
+            return None, None
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from Nominatim API: {str(e)}")
+        return None, None
+    
+
 @api_view(['GET'])
 @cache_page(60 * 5)  # Cache for 5 minutes
 def weather_info_api(request):
@@ -46,20 +66,16 @@ def weather_info_api(request):
     city = request.query_params.get('city', None)
         
     if city:
-        city = city.lower()
-        #lat = get_latitude(city)
-        #lon = get_longitude(city)
-        lat = request.query_params.get('lat', None)
-        lon = request.query_params.get('lon', None)
+        try:
+            lat, lon = get_lat_lon_from_nominatim(city)
+            if lat is None or lon is None:
+                return JsonResponse({'error': 'Invalid city or coordinates'}, status=400)
 
-        if lat is None or lon is None:
-            return JsonResponse({'error': 'Invalid city or coordinates'}, status=400)
-
-        API_KEY = get_api_key()
-        api_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
+            API_KEY = get_api_key()
+            api_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}'
         
 
-        try:
+        
             response = requests.get(api_url)
             response.raise_for_status()
             weather_data = response.json()
